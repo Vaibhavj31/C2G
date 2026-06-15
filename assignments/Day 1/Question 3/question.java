3c56d9fdfa4602bca67dbf85c0ce5c534ef518d6
Vaibhavj31 <vaibhavj549168@gmail.com>
2026-06-15 12:51:10 +0530

    [C2G] Submit: Day 1 - Question 3

---
+++ assignments/Day 1/Question 3/question.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int orderAmount = sc.nextInt();
+        if(orderAmount>=199){
+            System.out.println("Free Delivery");
+        }else{
+            System.out.println("Delivery Charges Applied");
+        }
+        
+    }
+}
